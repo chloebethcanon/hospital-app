cd4fe211722a7bd3e7a9b59e26d3714bbf565bdb
chloebethcanon <chloebethcanon@gmail.com>
2015-12-16 19:23:10 -0600

    generated Category model, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217005339) do
+ActiveRecord::Schema.define(version: 20151217012207) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title",       limit: 255
