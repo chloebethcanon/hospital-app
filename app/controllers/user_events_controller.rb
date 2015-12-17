c225d7024d0da604c28d7bc85e5d465e6686455c
chloebethcanon <chloebethcanon@gmail.com>
2015-12-16 21:18:31 -0600

    added user_events controller, added create method

---
+++ app/controllers/user_events_controller.rb
@@ -0,0 +1,11 @@
+class UserEventsController < ApplicationController
+
+  def create
+    @user_event = UserEvent.create(
+      user_id: current_user.id,
+      event_id: params[:event_id]
+      )
+    redirect_to "/events"
+  end
+
+end
