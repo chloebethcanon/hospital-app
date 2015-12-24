8deac3591d06067ec719e974b5ce8e626b650b18
chloebethcanon <chloebethcanon@gmail.com>
2015-12-23 21:18:47 -0600

    editing event show page to cater to admin, sign up, all events links showing up in incorrect spot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def index
     @all_events = Event.all
     @user_events = current_user.events
+    @owned_events = current_user.created_events
   end
 
 end
