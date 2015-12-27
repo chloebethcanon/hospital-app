0648e2a92af380fd587a9466666885789918af9b
chloebethcanon <chloebethcanon@gmail.com>
2015-12-27 16:10:03 -0600

    added service index, edit, new and show views, began index display and built create method

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,19 @@
 class ServicesController < ApplicationController
 
   def index
+    @services = Service.all
   end
 
   def new
   end
 
   def create
+    @service = Service.create(
+      title: params[:title],
+      category: params[:category],
+      description: params[:description]
+      )
+    redirect_to "/services"
   end
 
   def show
