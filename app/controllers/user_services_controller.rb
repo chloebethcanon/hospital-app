c9a812f8665b223c549aea154887488f5466be97
chloebethcanon <chloebethcanon@gmail.com>
2015-12-27 16:43:13 -0600

    added create and destroy methods to user_services controller

--- app/controllers/user_services_controller.rb
+++ app/controllers/user_services_controller.rb
@@ -1,9 +1,18 @@
 class UserServicesController < ApplicationController
 
   def create
+    @user_service = UserService.create(
+      user_id: current_user.id,
+      service_id: params[:service_id]
+      )
+    redirect_to "/pages"
   end
 
   def destroy
+    @user_service = UserService.find_by(id: params[:id])
+    @user_event.delete
+    flash[:success] = "You have successfully opted-out of this service."
+    redirect_to "/pages"
   end
   
 end
