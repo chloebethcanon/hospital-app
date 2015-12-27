01a8d6e66bc9f1b962adfbef368b9444a74882b5
chloebethcanon <chloebethcanon@gmail.com>
2015-12-27 16:23:50 -0600

    edited services show method and view

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -17,6 +17,13 @@ class ServicesController < ApplicationController
   end
 
   def show
+    @service = Service.find_by(id: params[:id])
+    if current_user && Service.find_by(id: params[:id]) == current_user.services.find_by(id: params[:id])
+      @current_user_opt_in = true
+      @user_servie = UserService.find_by(service_id: @service.id, user_id: current_user.id)
+    else
+      @current_user_opt_in = false
+    end
   end
 
   def edit
