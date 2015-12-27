1eaf3db3c3efd1c1d92dcaf185b6b71b3e0da0b3
chloebethcanon <chloebethcanon@gmail.com>
2015-12-27 15:13:31 -0600

    generated UserService model, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227210745) do
+ActiveRecord::Schema.define(version: 20151227211250) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 20151227210745) do
   create_table "user_roles", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "role_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "user_services", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "service_id", limit: 4
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
