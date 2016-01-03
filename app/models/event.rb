fb9ea879a1db6056e804fbe9b11963c50db99db6
chloebethcanon <chloebethcanon@gmail.com>
2016-01-03 17:04:24 -0600

    added readable date time methods to event model, updated event index page to incorporate theme html

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,16 @@ class Event < ActiveRecord::Base
   has_many :categorized_events
   has_many :categories, through: :categorized_events
   belongs_to :user
+
+  def readable_date(date)
+    if date
+      date.strftime("%A, %b %d, %Y")
+    end
+  end
+
+  def readable_time(time)
+    if time
+      time.strftime("%l:%M %p")
+    end
+  end
 end
