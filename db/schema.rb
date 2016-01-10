24a59a891a21cb9e6d169579aadeea8d46d681b5
chloebethcanon <chloebethcanon@gmail.com>
2016-01-10 11:01:31 -0600

    generated Message model, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227211250) do
+ActiveRecord::Schema.define(version: 20160110165943) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20151227211250) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.integer  "user_id",     limit: 4
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "subject",    limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "roles", force: :cascade do |t|
