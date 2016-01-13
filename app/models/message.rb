c8bd473d96997f0359458dc8596e5bc1cc3f9c3d
chloebethcanon <chloebethcanon@gmail.com>
2016-01-12 20:32:55 -0600

    added readable date time methods to message model and displayed in pages index view

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,14 @@ class Message < ActiveRecord::Base
 belongs_to :sender, class_name: "User", foreign_key: "user_id"
 has_many :message_recipients
 has_many :recipients, through: :message_recipients
+
+  def readable_date
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def readable_time
+    created_at.strftime("%l:%M %p")
+  end
+
 end
   
