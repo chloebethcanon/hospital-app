f04d9e11ef20ff54ed010145a9b60dacd2675a0d
chloebethcanon <chloebethcanon@gmail.com>
2016-01-13 19:16:27 -0600

    completed send message form, delete message functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,17 @@ class MessagesController < ApplicationController
   end
 
   def create
+    recipient = User.find_by(email: params[:email])
     @message = Message.create(
       subject: params[:subject],
-      body: params[:body]
+      body: params[:body],
+      user_id: current_user.id
       )
+    MessageRecipient.create(
+      message_id: @message.id,
+      user_id: recipient.id
+      )
+    redirect_to "/pages"
   end
 
   def show
@@ -24,6 +31,10 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+    @message = Message.find_by(id: params[:id])
+    @message.delete
+    MessageRecipient.where(message_id: params[:id])
+    redirect_to "/pages"
   end
   
 end
